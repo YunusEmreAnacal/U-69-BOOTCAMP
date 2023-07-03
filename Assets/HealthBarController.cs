d142c40a0b714a52ba2eea6f718b09940acf7364
KemalAkkurt <kemalakkurt1@gmail.com>
2023-07-03 16:43:53 +0300

    karakterfalldamage

---
+++ Assets/HealthBarController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public Slider healthSlider; 
+    public FallDamage FallDamageScript; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FallDamageScript.can = 100;
+        healthSlider.maxValue = FallDamageScript.can; 
+        healthSlider.value = FallDamageScript.can; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    void UpdateHealthBar()
+    {
+        if (healthSlider.value > FallDamageScript.can)
+        {
+            healthSlider.value = FallDamageScript.can; 
+        }
+        else if (healthSlider.value < FallDamageScript.can)
+        {
+            healthSlider.value -= Time.deltaTime * 10f; 
+        }
+    }
+}
